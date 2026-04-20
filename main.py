7c1369c44e93a033c4e6cb4284ed6271bc243804
acapodanno <alessandro.capodanno95@gmail.com>
2026-04-20 19:34:19 +0200

    refactoring code

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from scripts.ingest import retrieve_documents, chunk_documents
-from scripts.retriever import store_and_retrieve
+from scripts.retriever import build_vector_store, get_retriever
 from scripts.rag_chain import setup_chain
 
 if __name__ == "__main__":
     documents = retrieve_documents()
     chunked_documents = chunk_documents(documents)
-    retriever = store_and_retrieve(chunked_documents)
-    rag_chain = setup_chain(retriever)
+    build_vector_store(chunked_documents)
+    rag_chain = setup_chain(get_retriever())
     question = "Come posso richiedere un permesso di lavoro da remoto?"
     answer = rag_chain.invoke({"input": question})
     print("Answer:", answer["answer"])
+    for doc in answer["context"]:
+        print(f"  📄 {doc.metadata.get('source')}")
--- scripts/retriever.py
+++ scripts/retriever.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Dense search
-def store_and_retrieve(chunked_documents: list[Document]):
+def build_vector_store(chunked_documents: list[Document]):
     vector_store = Chroma.from_documents(
         documents=chunked_documents, 
         embedding=OpenAIEmbeddings(
@@ -16,7 +16,15 @@ def store_and_retrieve(chunked_documents: list[Document]):
         persist_directory="./chroma_db",
         collection_name="data_pulse",
     )
+    return vector_store
+
+def get_retriever():
+    vector_store = Chroma(
+        persist_directory="./chroma_db",
+        collection_name="data_pulse",
+        embedding_function=OpenAIEmbeddings(...),
+    )
     return vector_store.as_retriever(
-        search_type="mmr", # Max Marginal Relevance
+        search_type="mmr",
         search_kwargs={'k': 3, 'fetch_k': 10}
-        )
+    )
