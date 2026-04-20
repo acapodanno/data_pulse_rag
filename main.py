004756354d44533a5f1fbca4eec7ec4020e099ee
acapodanno <alessandro.capodanno95@gmail.com>
2026-04-20 10:09:45 +0200

    add .

--- main.py
+++ main.py
@@ -5,4 +5,3 @@ if __name__ == "__main__":
     documents = retrieve_documents()
     chunked_documents = chunk_documents(documents)
     retriever = store_and_retrieve(chunked_documents)
-    print(retriever)
