d0e2182d46d072ca6817f4e565b3ea1dfd1305c6
acapodanno <alessandro.capodanno95@gmail.com>
2026-04-20 19:35:17 +0200

    optimized

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from scripts.ingest import retrieve_documents, chunk_documents
 from scripts.retriever import build_vector_store, get_retriever
 from scripts.rag_chain import setup_chain
+import os
 
 if __name__ == "__main__":
-    documents = retrieve_documents()
-    chunked_documents = chunk_documents(documents)
-    build_vector_store(chunked_documents)
+    if not os.path.exists("./chroma_db"):
+        documents = retrieve_documents()
+        chunked_documents = chunk_documents(documents)
+        build_vector_store(chunked_documents)
     rag_chain = setup_chain(get_retriever())
     question = "Come posso richiedere un permesso di lavoro da remoto?"
     answer = rag_chain.invoke({"input": question})
