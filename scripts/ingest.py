82e35c3ea501f3c3f3cbdc9746fd40c5136fbdc8
acapodanno <alessandro.capodanno95@gmail.com>
2026-04-20 19:25:27 +0200

    remove import unused

--- scripts/ingest.py
+++ scripts/ingest.py
@@ -1,4 +1,3 @@
-from langchain_community.utilities.pebblo import LOADER_TYPE_MAPPING
 from langchain_community.document_loaders import UnstructuredMarkdownLoader, UnstructuredPDFLoader, UnstructuredWordDocumentLoader, UnstructuredFileLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
